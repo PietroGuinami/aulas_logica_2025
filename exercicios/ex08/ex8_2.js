764283d73dbece2c5d39e357c3a8ec7051add4cc
PG <pietro.loureiro@aluno.senai.br>
2025-04-14 12:33:54 -0300

    exercicios

---
+++ exercicios/ex08/ex8_2.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')()
+// Crie uma lista com os nomes de três amigos e imprima cada nome.
+
+    let nomes = ['Paulo', 'Cadu', '...']
+
+    if(console.log(nomes)){}
+        
---
+++ exercicios/ex08/ex8_3.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')()
+// Crie uma lista com os meses e imprima apenas os meses que começam com "J".
+''
+        let m = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 
+                'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+
+                if(console.log(m)){}
